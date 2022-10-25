ae670662b622bb7964c251ec70223bd329cbfe69
Eric <whodatbio@gmail.com>
2022-10-25 15:19:55 -0400

    saving

--- src/components/pages/aboutMe.js
+++ src/components/pages/aboutMe.js
@@ -8,7 +8,7 @@ export default function AboutMe() {
             <div className='d-flex justify-content-center'>
                 <img
                     className = 'rounded img-thumbnail mt-5 me-5'
-                    width = "500"
+                    width = "300"
                     src = { PortPic }
                     alt = 'Picture of Eric Courter'/>
                 <p className='align-self-center pt-5 ms-5'>My name is Eric Courter and I am a Full-Stack Web Developer. I am ready and eager to learn more about coding applications. I am in the process of creating as many projects as I can so I can further increase my knowledge and understanding of Computer Science.</p>
